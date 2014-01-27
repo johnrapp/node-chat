70be4422e6d6e1b07dc316924035e4a0d96e2263
John Rapp <johnfarnes@hotmail.com>
2014-01-27 21:05:30 +0100

    Kinda big update

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,57 @@
 var app = angular.module('app', ['socket.io'])
+//.constant('alphabet', ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+.constant('vowels', ['a', 'e', 'i', 'o', 'u', 'y'])
+.constant('consonants', ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'])
+
 .constant('messages', [])
-.factory('nickname', function() {
-	var nickname = '';
-	return function(value) {
-		if(value) nickname = value;
-		else return nickname
+.constant('profile', {})
+.service('Color', function() {
+	return function(name, backgroundColor, textColor) {
+		this.style = {}
+		this.style['background-color'] = backgroundColor;
+		this.style['color'] = textColor;
+		this.name = name;
 	}
 })
 
-.controller('nicknameCtrl', ['$scope', 'nickname',
-	function($scope, nickname) {
+.controller('nicknameCtrl', ['$scope', 'profile', 'consonants', 'vowels',
+	function($scope, profile, consonants, vowels) {
 		$scope.$watch('nickname', function() {
-			nickname($scope.nickname);
+			profile.nickname = $scope.nickname;
 		});
-		$scope.randomName = function() {
+		$scope.random = function() {
+			$scope.nickname = function generate(string, amount) {
+				var array = Math.random() > 0.6 ? consonants : vowels;
+				string += array[Math.floor(Math.random() * array.length)];
+				if(amount > 0) return generate(string, --amount);
+				else return string;
+			}('', Math.floor(Math.random() * 4) + 2);
+		}
+		$scope.clear = function() {
+			$scope.nickname = '';
+		}
+	}]
+)
+.controller('colorCtrl', ['$scope', 'profile', 'Color',
+	function($scope, profile, Color) {
+		$scope.colors = [
+			new Color('Gray', '#222', '#fff'),
+			new Color('Green', '#08FD00', '#fff'),
+			new Color('Silver', '#DDDDDD', '#000'),
+			new Color('Gold', '#FFD700', '#000'),
+			new Color('Blue', '#3377C2', '#fff'),
+			new Color('Red', '#BA0001', '#fff'),
+			new Color('White', '#fff', '#000')
+		];
+		$scope.current = $scope.colors[0];
+		profile.style = $scope.current.style;
+		$scope.toggle = function() {
+			$scope.expanded = !$scope.expanded;
+		}
+		$scope.select = function(color) {
+			profile.style = color.style;
+			$scope.current = color;
+			$scope.expanded = false;
 		}
 	}]
 )
@@ -21,20 +59,22 @@ var app = angular.module('app', ['socket.io'])
 	function($scope, messages, socket) {
 		$scope.messages = messages;
 		socket.on('new message', function(message) {
-			message.own = false;
 			messages.push(message);
 		});
 	}]
 )
-.controller('newCtrl', ['$scope', 'messages', 'socket', 'nickname',
-	function($scope, messages, socket, nickname) {
+.controller('newCtrl', ['$scope', 'messages', 'socket', 'profile',
+	function($scope, messages, socket, profile) {
 		var MAX_LENGTH = 128;
 
 		$scope.keyDown = function(e) {
 			if(!$scope.message) return false;
 			if(e.keyCode === 13 && !e.shiftKey) {
-				messages.push({content: $scope.message, own: true});
-				socket.emit('broadcast message', {content: $scope.message, author: nickname() || 'anonymous'});
+				var message = {content: $scope.message, sender: profile};
+				socket.emit('broadcast message', message);
+				message.own = true;
+				messages.push(message);
+				
 				$scope.message = '';
 				e.preventDefault();	
 			} else if($scope.message.length >= MAX_LENGTH && e.keyCode !== 8)
